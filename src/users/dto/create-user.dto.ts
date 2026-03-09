945820263a910ad17fba30b47250398b4d4e38d6
Abishek Kagra <abhishek.kagra@woco.co.in>
2026-03-09 12:32:31 +0530

    Update

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,27 @@
+import { IsEmail, IsNotEmpty, IsObject, IsOptional, IsBoolean, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsOptional()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsObject()
+  @IsOptional()
   role: { dev: boolean; cs: boolean };
+
+  @IsBoolean()
+  @IsOptional()
   admin: boolean;
+
+  @IsObject()
+  @IsOptional()
   write?: { dev: boolean; cs: boolean };
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsObject, IsOptional, IsBoolean, IsString } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
   name?: string;
+
+  @IsObject()
+  @IsOptional()
   role?: { dev: boolean; cs: boolean };
+
+  @IsBoolean()
+  @IsOptional()
   admin?: boolean;
+
+  @IsObject()
+  @IsOptional()
   write?: { dev: boolean; cs: boolean };
 }
